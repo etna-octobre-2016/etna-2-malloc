a7978258ce207da288c611c0e01117f7f8352499
Said Ahemt <ahemt_s@ItadakiBook.local>
2015-04-17 23:10:34 +0200

    reorganizes headers

---
+++ src/headers/structures.h
@@ -0,0 +1,25 @@
+#ifndef _MALLOC_STRUCT_H
+#define _MALLOC_STRUCT_H
+
+  struct                    s_malloc_chunks
+  {
+    bool                    is_free;
+    void                    *bin;
+    void                    *ptr;
+    struct s_malloc_chunks  *next;
+  };
+  struct                    s_malloc_bins
+  {
+    int                     free_chunks;
+    size_t                  size;
+    struct s_malloc_chunks  *chunks;
+    struct s_malloc_bins    *next;
+  };
+  struct                    s_malloc_data
+  {
+    bool                    is_initialized;
+    void                    *base_data_segment_addr;
+    struct s_malloc_bins    *bins;
+  };
+
+#endif
---
+++ src/headers/types.h
@@ -0,0 +1,8 @@
+#ifndef _MALLOC_TYPES_H
+#define _MALLOC_TYPES_H
+
+  typedef struct s_malloc_bins    t_malloc_bins;
+  typedef struct s_malloc_chunks  t_malloc_chunks;
+  typedef struct s_malloc_data    t_malloc_data;
+
+#endif
